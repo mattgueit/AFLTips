21a3e6f9c483d440218983e17ce9e382dccde0f6
Matt Gueit <mattgueit@gmail.com>
2021-04-27 20:33:58 +1000

    Rename team logos to ID instead of ABBV; Add a little bit of formatting to the ladder (still incomplete).

--- AFLTips/Server/Services/LadderService.cs
+++ AFLTips/Server/Services/LadderService.cs
@@ -1,15 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Net.Http.Headers;
 using System.Net.Http;
 using System.Threading.Tasks;
 using AFLTips.Shared.DataModels;
 using Newtonsoft.Json;
 
+
 namespace AFLTips.Server.Services
 {
     public class LadderService : ILadderService
     {
-        private static readonly HttpClient _httpClient = new HttpClient();
+        private readonly HttpClient _httpClient;
+
+        public LadderService()
+        {
+            _httpClient = new HttpClient();
+            //_httpClient.DefaultRequestHeaders.UserAgent.Add(new ProductInfoHeaderValue("https://github.com/mattgueit/AFLTips"));
+        }
 
         public async Task<Ladder> GetLadder()
         {
